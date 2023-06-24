bfd127618befe27269003cb63cc851e30f1a945e
Sebastian Graf <96569041+Wasterix@users.noreply.github.com>
2023-06-24 13:31:06 +0200

    Klassen richtig verschoben; NBASeasonViewer enthält main; Nicht funktionierende Tests gelöscht.

--- src/main/java/outdated/Get_all_Teams.java
+++ src/main/java/outdated/Get_all_Teams.java
@@ -1,4 +1,4 @@
-package project;
+package outdated;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
--- src/main/java/outdated/Id_to_Teamname.java
+++ src/main/java/outdated/Id_to_Teamname.java
@@ -1,4 +1,4 @@
-package project;
+package outdated;
 
 import java.io.BufferedReader;
 import java.io.IOException;
