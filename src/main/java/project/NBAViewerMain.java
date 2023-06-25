da08aa58eca267d3406616f0a2ca4f692d2cbd78
Sebastian Graf <se.graf@bora.com>
2023-06-25 17:23:07 +0200

    Outdated komplett gelöscht, Code läuft

--- src/main/java/project/NBAViewerMain.java
+++ src/main/java/project/NBAViewerMain.java
@@ -1,25 +1,47 @@
 package project;
 
-import outdated.NBASeasonViewer;
+import com.sun.net.httpserver.HttpServer;
+import project.GamesHandler;
+import project.InputHandler;
+import project.LookupTable;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class NBAViewerMain {
     public static void main(String[] args) {
-        NBASeasonViewer viewer = new NBASeasonViewer();
-        viewer.run();
-    }
-/*
-    public void run() {
         InputHandler inputHandler = new InputHandler();
-        DataProcessor dataProcessor = new DataProcessor();
-        HttpServerManager httpServerManager = new HttpServerManager();
 
         String inputName = inputHandler.getInputName();
         String inputSeason = inputHandler.getInputSeason();
 
         String teamId = LookupTable.getTeamId(inputName);
-        String year = dataProcessor.calculateYear(inputSeason);
+        String year = calculateYear(inputSeason);
 
-        dataProcessor.processInput(inputName, teamId, inputSeason, year);
-        httpServerManager.startHttpServer(teamId, inputSeason, year);
-    }*/
+        processInput(inputName, teamId, inputSeason, year);
+        startHttpServer(teamId, inputSeason, year, inputName);
+
+    }
+
+    private static String calculateYear(String inputSeason) {
+        int number = Integer.parseInt(inputSeason);
+        return String.valueOf(number + 1);
+    }
+
+    private static void processInput(String inputName, String teamId, String inputSeason, String year) {
+        System.out.println("Die Id der " + inputName + " lautet: " + teamId);
+        System.out.println("Ausgewählte Saison: " + inputSeason + "/" + year);
+    }
+
+    private static void startHttpServer(String teamId, String inputSeason, String year, String inputName) {
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server.createContext("/id=" + teamId + "&season=" + inputSeason + "/" + year, new GamesHandler(teamId, inputSeason, inputName));
+            server.start();
+            System.out.println("HTTP-Server läuft auf http://localhost:8000/id=" + teamId + "&season=" + inputSeason + "/" + year);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
